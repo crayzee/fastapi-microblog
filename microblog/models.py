81cbb0b43ab90c93e52a9b71a7516c05638d426a
crayzee <twopik@gmail.com>
2021-02-06 10:08:41 +0300

    add microblog: schemas, service, endpoints, routes

--- microblog/models.py
+++ microblog/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from user.models import User
 
 from core.db import Base
 
@@ -10,4 +11,4 @@ class Post(Base):
     text = Column(String(length=350))
     date = Column(DateTime)
     user = Column(Integer, ForeignKey("user.id"))
-    user_id = relationship("User")
+    user_id = relationship(User)
---
+++ routes.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+from microblog import blog
+
+routes = APIRouter()
+
+routes.include_router(blog.router, prefix="/blog")
